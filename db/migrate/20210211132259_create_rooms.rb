c9d897805248dadf5a1a12281fd3a8aa93244d06
taiki <boruboredo@gmail.com>
2021-02-11 23:14:50 +0900

    カテゴリを追加

--- db/migrate/20210211132259_create_rooms.rb
+++ db/migrate/20210211132259_create_rooms.rb
@@ -2,10 +2,14 @@ class CreateRooms < ActiveRecord::Migration[6.1]
   def change
     create_table :rooms do |t|
       t.string :title
+      t.string :room_identity
       t.string :description
       t.integer :max_user_num
+      t.string :main_language
 
       t.timestamps
     end
+    add_foreign_key :rooms, :users, column: :admin_usesr_id
+    add_foreign_key :rooms, :categories, column: :category_id
   end
 end
