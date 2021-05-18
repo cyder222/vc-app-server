b296716a730ceaf3fb4bd4cef2bb2c55016569d7
taiki <boruboredo@gmail.com>
2021-05-19 08:26:42 +0900

    delete specを修正

--- spec/controllers/requests/api/rooms/users/delete_spec.rb
+++ spec/controllers/requests/api/rooms/users/delete_spec.rb
@@ -4,7 +4,6 @@ require 'rails_helper'
 
 RSpec.describe 'DELETE /api/rooms/:id/users', type: :request do
     subject { delete "/api/rooms/#{room.id}/users/#{join_user.id}", params: params.to_json, headers: headers }
-
     let(:category) { FactoryBot.create(:category) }
     let(:admin_user) { FactoryBot.create(:user) }
     let(:join_user) { FactoryBot.create(:user) }
@@ -19,13 +18,18 @@ RSpec.describe 'DELETE /api/rooms/:id/users', type: :request do
     } }
   
     context 'delete api rooms-users' do
-        it 'success' do   
+        it 'success' do
             subject
             expect(response).to have_http_status(:ok)      
         end
         it 'changed' do
             RoomUser.join(room: room, user: join_user)
             expect{subject}.to change{ Room.find_by(id: room.id).users.length}.from(1).to(0)
+        end
+
+        it 'fail on api user is not same as :id user' do
+            delete "/api/rooms/#{room.id}/users/#{another_user.id}", params: params.to_json, headers: headers
+            expect(response).to have_http_status(:bad_request)
         end
     end
   end
