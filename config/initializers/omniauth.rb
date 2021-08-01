6e3b941318b53163d4e974803d7a6d90b4bf8c69
taiki <boruboredo@gmail.com>
2021-08-02 02:23:21 +0900

    ENVの名前をauthからoauthに変更

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-    auth_key = ENV['google_auth_key']
-    auth_secret = ENV['google_auth_secret']
+    auth_key = ENV['google_oauth_key']
+    auth_secret = ENV['google_oauth_secret']
     provider :google_oauth2, auth_key, auth_secret,
              name: :google_oauth2, scope: %w(profile)
 end
