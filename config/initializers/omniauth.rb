230bf004d35753b268dc2cd597ff0475c7065e6a
taiki <boruboredo@gmail.com>
2021-08-02 02:17:33 +0900

    シンタックスエラー対応

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  # frozen_string_literal: true
-
-Rails.application.config.middleware.use OmniAuth::Builder do
     auth_key = ENV['google_auth_key']
     auth_secret = ENV['google_auth_secret']
     provider :google_oauth2, auth_key, auth_secret,
