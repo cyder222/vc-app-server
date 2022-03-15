41aa7ffaed585235fbddb444fd092a0d2b660505
taiki <boruboredo@gmail.com>
2022-03-16 06:52:41 +0900

    omniauth.origin（return_to)に対応した

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,7 @@ class SessionsController < ApplicationController
 
   def create
     return Rails.logger.error('omaniauth.auth is empty') if request.env['omniauth.auth'].empty?
+    origin = request.env['omniauth.origin'] || Rails.configuration.x.frontend
 
     user = User.from_omniauth(request.env['omniauth.auth'])
     return Rails.logger.error("invalid omniauth parameter#{request.env['omniauth.auth']}") if user.nil?
@@ -13,7 +14,7 @@ class SessionsController < ApplicationController
       flash[:refresh_token] = user.oauth_refresh_token
       flash[:uid] = user.id
 
-      redirect_to "#{Rails.configuration.x.frontend}?uid=#{user.uid}&auth_token=#{user.oauth_token}"
+      redirect_to "#{origin}?uid=#{user.uid}&auth_token=#{user.oauth_token}"
     else
       redirect_to new_session_path
     end
